28a8b5b524fde8f4fe327d22a79d336968326367
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-03-04 13:07:13 -0600

    working-with-bootstrap-icons

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,5 +1,8 @@
 <?php
 
 function hemauto_enqueue() {
-
+    wp_register_style(
+        'hemauto_font_tiltWrap_play_urbanist_and_roadRage',
+        'https://fonts.googleapis.com/css2?family=Play:wght@400;700&family=Road+Rage&family=Tilt+Warp&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap'
+    );
 }
