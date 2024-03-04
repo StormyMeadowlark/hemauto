293bb64996224631e9d83ccfce5a834a20665e49
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-03-04 15:08:53 -0600

    loading stylesheets into wordpress from a static html template

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
 
 // Includes
 include(get_theme_file_path( '/includes/front/enqueue.php' ));
-
+include(get_theme_file_path( '/includes/front/head.php' ));
 // Hooks
 add_action( 'wp_enqueue_scripts', 'hemauto_enqueue' );
+add_action( 'wp_head', 'hemauto_head', 5);
--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -3,6 +3,22 @@
 function hemauto_enqueue() {
     wp_register_style(
         'hemauto_font_tiltWrap_play_urbanist_and_roadRage',
-        'https://fonts.googleapis.com/css2?family=Play:wght@400;700&family=Road+Rage&family=Tilt+Warp&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap'
+        'https://fonts.googleapis.com/css2?family=Play:wght@400;700&family=Road+Rage&family=Tilt+Warp&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap',
+        [],
+        null
     );
+
+    wp_register_style(
+        'hemauto_bootstrap_icons',
+        get_theme_file_uri( 'assets\icons-main\font\bootstrap-icons.css' )
+    );
+
+    wp_register_style(
+        'hemauto_theme',
+        get_theme_file_uri( 'assets\Flexbox-Navigation-Menu.css' )
+    );
+
+    wp_enqueue_style( 'hemauto_font_tiltWrap_play_urbanist_and_roadRage' );
+    wp_enqueue_style( 'hemauto_bootstrap_icons' );
+    wp_enqueue_style( 'hemauto_theme' );
 }
